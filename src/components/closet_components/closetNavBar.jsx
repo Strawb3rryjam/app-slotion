0cee4cfed787b5138363c324f122fb5b2869c4de
Jamille <Vicentee.jamille@gmail.com>
2025-05-18 03:52:32 -0700

    styling for create outfit

--- src/components/closet_components/closetNavBar.jsx
+++ src/components/closet_components/closetNavBar.jsx
@@ -8,34 +8,40 @@ Object so we can pass in different props
 * top right: button (e.g. save outfit)
 */
 
+import styles from '../../css/closetPage.module.css';
 
-
-import styles from "../../css/closetPage.module.css";
-
-const ClosetNavBar = ({ 
-  leftButtonImgSrc, 
-  leftButtonAlt = "Close", 
-  onLeftButtonClick, 
-  title, 
-  rightButtonText, 
-  onRightButtonClick 
+const ClosetNavBar = ({
+  leftButtonImgSrc,
+  leftButtonAlt = "Back",
+  onLeftButtonClick,
+  title,
+  rightButtonText,
+  onRightButtonClick,
+  isActive = true
 }) => {
   return (
-    <nav className={styles.navbar}>
-      <button 
-        className={styles.leftButton} 
+    <nav className={styles.navBar}>
+      <button
+        className={styles.navBar_leftButton}
         onClick={onLeftButtonClick}
         aria-label={leftButtonAlt}
       >
-        <img src={leftButtonImgSrc} alt={leftButtonAlt} className={styles.leftButtonImg} />
+        <img src={leftButtonImgSrc} alt={leftButtonAlt} className={styles.navBar_leftButtonImg} />
       </button>
-      <h1 className={styles.title}>{title}</h1>
-      <button 
-        className={styles.rightButton} 
-        onClick={onRightButtonClick}
+
+      <h1 className={styles.navBar_title}>{title}</h1>
+
+          <button
+        className={`
+          ${styles.navBar_rightButton}
+          ${isActive ? styles.navBar_rightButton_active : styles.navBar_rightButton_disabled}
+        `}
+        onClick={isActive ? onRightButtonClick : null}
+        disabled={!isActive}
       >
         {rightButtonText}
       </button>
+
     </nav>
   );
 };
--- src/pages/Closet.jsx
+++ src/pages/Closet.jsx
@@ -1,7 +1,13 @@
 
+/*styles*/
+import styles from '../css/closetPage.module.css';
+
 /*Components*/
 import ImageGallery from '../components/closet_components/closetGallery'; // Make sure the path is correct
 import CreateOutfitButton from '../components/closet_components/createOutfitButton';
+
+/*Images*/
+import HangerImage from'../assets/Hanger.svg';
 
 
 function Closet() {
@@ -9,11 +15,9 @@ function Closet() {
 
   return (
     <div>
-
-      
-      <div className="closet-header">
-            {/*<img src={} alt="Hanger" className="closet-image" />*/}
-            <h1 id='closetTitle'>Closet</h1>
+      <div className={styles["closetHeader"]}>
+            <img src={HangerImage} alt="Hanger" className={styles['closetHeader_image']} />
+            <h1 id={styles['closetHeader_title']}>Closet</h1>
             <CreateOutfitButton></CreateOutfitButton>
       </div>
     </div>
