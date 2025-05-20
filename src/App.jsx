b049dc40628e38ffb4d66c7a97622bbda3f1ddcb
Jamille <Vicentee.jamille@gmail.com>
2025-05-19 19:12:48 -0700

    scrollable feature to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import './index.css';
 import { Routes, Route } from 'react-router-dom';
 
 /* Pages */
@@ -8,24 +9,30 @@ import Profile from './pages/Profile';
 import CreateOutfit from './pages/CreateOutfit.jsx';
 import Onboarding from './pages/Onboarding.jsx';
 import Login from './pages/Login.jsx';
-import AddItem from './pages/AddItem.jsx';      
+import AddItem from './components/closet_components/addItem.jsx';      
 import SaveOutfit from './pages/SaveOutfit.jsx'; 
 
 function App() {
   return (
     <div className="app-container">
-      <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/closet" element={<Closet />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/create-outfit" element={<CreateOutfit />} />
-          <Route path="/add-item" element={<AddItem />} />
-          <Route path="/save-outfit" element={<SaveOutfit />} />
-          <Route path="/onboarding" element={<Onboarding />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </main>
+      <div className="scroll-container">
+        <div className="wrapper">
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/closet" element={<Closet />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/create-outfit" element={<CreateOutfit />} />
+              <Route path="/add-item" element={<AddItem />} />
+              <Route path="/save-outfit" element={<SaveOutfit />} />
+              <Route path="/onboarding" element={<Onboarding />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </main>
+        </div>
+      </div>
+      {/* Note: Your navbar component should be placed here outside the scroll-container */}
+      {/* If you have a separate Navbar component, include it like: <Navbar /> */}
     </div>
   );
 }
