6b0d95e11dc8536e534de2ecb5007b86eff08b49
Jamille <Vicentee.jamille@gmail.com>
2025-05-20 20:58:12 -0700

    fixed route paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import './index.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'; 
 
 /* Pages */
 import Home from './pages/Home';
@@ -9,31 +9,38 @@ import Profile from './pages/Profile';
 import CreateOutfit from './pages/CreateOutfit.jsx';
 import Onboarding from './pages/Onboarding.jsx';
 import Login from './pages/Login.jsx';
-import AddItem from './components/closet_components/addItem.jsx';      
-import SaveOutfit from './pages/SaveOutfit.jsx'; 
+import AddItem from './components/closet_components/addItem.jsx';
+import AddNewItemPopup from './components/closet_components/addNewItem.jsx';
+import SaveOutfit from './pages/SaveOutfit.jsx';
+import SlotionLibrary from './components/closet_components/slotionLibrary.jsx';
+import AddPhotos from './components/closet_components/addPhotos.jsx';
+import AddItemForm from './components/closet_components/newItemForm.jsx';
+import AddItemFormComplete from './components/closet_components/newItemFormComplete.jsx';
 
 function App() {
   return (
     <div className="app-container">
-      <div className="scroll-container">
-        <div className="wrapper">
-          <main className="main-content">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/closet" element={<Closet />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/create-outfit" element={<CreateOutfit />} />
-              <Route path="/add-item" element={<AddItem />} />
-              <Route path="/save-outfit" element={<SaveOutfit />} />
-              <Route path="/onboarding" element={<Onboarding />} />
-              <Route path="/login" element={<Login />} />
-            </Routes>
-          </main>
-        </div>
-      </div>
-     
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/onboarding" element={<Onboarding />} />
+          <Route path="/login" element={<Login />} />
+
+          {/* Closet Page */}
+          <Route path="/closet" element={<Closet />} />
+          <Route path="/create-outfit" element={<CreateOutfit />} />
+          <Route path="/add-item" element={<AddItem />} />
+          <Route path="/add-item-form" element={<AddItemForm />} />
+          <Route path="/add-new-item" element={<AddNewItemPopup />} />
+          <Route path="/add-new-item-complete" element={<AddItemFormComplete />} />
+          <Route path="/save-outfit" element={<SaveOutfit />} />
+          <Route path="/slotion-library" element={<SlotionLibrary />} />
+          <Route path="/add-photo" element={<AddPhotos />} />
+        </Routes>
+      </main>
     </div>
   );
-};
+}
 
 export default App;
